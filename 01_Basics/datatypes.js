393059157b76897e9e06604b1cfceed9f6838ac5
Bhavy patel <88310262+Bhavy1800@users.noreply.github.com>
2023-11-26 16:30:11 +0000

    datatypes and its conversion

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,18 @@
+let score = "100abc";
+// let score = "null";
+// let score = true;
+// let score = "100";
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+let stringNumber = String(score)
+console.log(stringNumber);
+
+
+
